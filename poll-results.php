ab92f27a775ad4999382763ca089f7c1f4e86d4d
Max Bartels <max.002@web.de>
2022-08-23 14:28:34 +0200

    found bug in cleaning code

--- poll-results.php
+++ poll-results.php
@@ -15,7 +15,7 @@ $base_page = 'admin.php?page='.$base_name;
 	<table class="form-table">
 		<tbody>
 		<tr>
-			<th width="20%" scope="row" valign="top"> <?php echo clean_votes();?></th>
+			<th width="20%" scope="row" valign="top"> <?php clean_votes();?></th>
 			<td width="80%"><?php echo polls_archive(1);?></td>
 		</tr>
 	</table>
